e905af552f638a27e052f3be6067cadbe29baf80
Piotr Lipkowski <piotr.lipkowsky@gmail.com>
2026-06-17 13:51:41 +0200

    viewport fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
@@ -6,6 +6,13 @@ import "./globals.css";
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false, // Blokuje automatyczny i wymuszony zoom
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
