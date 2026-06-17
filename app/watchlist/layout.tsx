ea750f6ec5c3aadd36862d410b5acd9761e58f41
Piotr Lipkowski <piotr.lipkowsky@gmail.com>
2026-06-17 13:44:45 +0200

    ux fix

--- app/watchlist/layout.tsx
+++ app/watchlist/layout.tsx
@@ -11,9 +11,9 @@ export default function WatchlistLayout({
 }) {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="flex-1 w-full flex flex-col gap-10 md:gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-4 md:px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               <Link href={"/"}>PocketCinema</Link>
             </div>
@@ -22,11 +22,12 @@ export default function WatchlistLayout({
             </Suspense>
           </div>
         </nav>
-        <div className="flex flex-col w-full max-w-5xl px-4 md:px-0">
+        <div className="flex flex-col w-full max-w-5xl px-4 sm:px-6 md:px-8 box-border">
           {children}
         </div>
+          
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-8 md:py-16 mt-auto">
           <p className="font-bold hover:underline">
             Powered by Piotr Lipkowski
           </p>
